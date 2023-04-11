c53dd1764472932ca7635ded470078b34749f367
glorystar0529 <114935743+glorystar0529@users.noreply.github.com>
2023-04-10 20:54:09 -0600

    Created files "properly".

---
+++ Agario/TowardAgarioStepOne/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TowardAgarioStepOne;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
