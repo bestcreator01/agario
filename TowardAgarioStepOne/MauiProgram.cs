75a246eebc6728da771c82fb3a51802a6c7e2940
seointhenerd <u1324614@gcloud.utah.edu>
2023-04-10 22:48:33 -0600

    Finished step one with the error where the information about a circle does not show.

--- TowardAgarioStepOne/MauiProgram.cs
+++ TowardAgarioStepOne/MauiProgram.cs
@@ -4,21 +4,24 @@ namespace TowardAgarioStepOne;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
-
-#if DEBUG
-		builder.Logging.AddDebug();
-#endif
-
-		return builder.Build();
-	}
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+            //.Services.AddLogging(configure =>
+            //{
+            //    configure.AddConsole();
+            //    configure.AddDebug();
+            //    configure.AddProvider(new FileLoggerProvider());
+            //    configure.SetMinimumLevel(LogLevel.Debug);
+            //})
+            //.AddTransient<MainPage>();
+        return builder.Build();
+    }
 }
